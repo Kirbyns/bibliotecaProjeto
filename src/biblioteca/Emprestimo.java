88dfb6adc76805a619f6b26e796900b7118aa715
Bruno Vicente <brunovicente.dev@gmail.com>
2024-05-20 09:47:06 -0300

    Added status

--- src/biblioteca/Emprestimo.java
+++ src/biblioteca/Emprestimo.java
@@ -1,13 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.biblioteca.models;
-
-/**
- *
- * @author moasa
- */
+package biblioteca;
 
 import java.time.LocalDate;
 
@@ -15,14 +6,16 @@ public class Emprestimo {
     private int id;
     private LocalDate dataRetirada;
     private LocalDate dataDevolucao;
+    private String status;
     private int idExemplar;
     private int idUsuario;
 
     // Construtor
-    public Emprestimo(int id, LocalDate dataRetirada, LocalDate dataDevolucao, int idExemplar, int idUsuario) {
+    public Emprestimo(int id, LocalDate dataRetirada, LocalDate dataDevolucao, String status, int idExemplar, int idUsuario) {
         this.id = id;
         this.dataRetirada = dataRetirada;
         this.dataDevolucao = dataDevolucao;
+        this.status = status;
         this.idExemplar = idExemplar;
         this.idUsuario = idUsuario;
     }
@@ -50,6 +43,14 @@ public class Emprestimo {
 
     public void setDataDevolucao(LocalDate dataDevolucao) {
         this.dataDevolucao = dataDevolucao;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
     }
 
     public int getIdExemplar() {
